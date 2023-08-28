5157d0a316cc6c8548034734c801bee7684dbec1
mopber <98894226+mopber@users.noreply.github.com>
2023-08-28 15:10:21 +0700

    Add files via upload

--- box.js
+++ box.js
@@ -1,14 +1,15 @@
-function rollButton() {
-    console.log("ggg")
-    // alert("jjj");
-    // for (var i = 1; i <= 3; i++) {
-    //   var rollResult = Math.floor(Math.random() * 6) + 1;
-    //   var imageID = "dice" + i; // Assuming IDs are "dice1", "dice2", "dice3"
-      
-    //   var imgElement = document.getElementById(imageID);
-    //   if (imgElement) {
-    //     imgElement.setAttribute("src", rollResult + ".png");
-    //   }
-    // }
+function rollButtonWithAnimation() {
+    var i = 1;
+    var interval = setInterval(function() {
+      var rollResult = Math.floor(Math.random() * 6) + 1;
+      var imageID = "dice" + i;
+      var imgElement = document.querySelector("." + imageID);
+      if (imgElement) {
+        imgElement.setAttribute("src", "./img/" + rollResult + ".png");
+      }
+      i++;
+      if (i > 3) {
+        clearInterval(interval);
+      }
+    }, 500); 
   }
-  
